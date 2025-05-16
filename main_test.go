cd2e499b519e23af921efc620b7a57a8452f7d0a
BPplays <andymalbp@gmail.com>
2025-05-16 10:30:41 -0700

    test

--- main_test.go
+++ main_test.go
@@ -23,7 +23,41 @@ func TestSprintSingleTz(t *testing.T) {
 	})
 
 
-	t.Run("print single tz 50:", func(t *testing.T) {
+	t.Run("print single tz 125:", func(t *testing.T) {
+		t.Parallel()
+		strs := []string{}
+
+		rang := 125
+		if testing.Short() {
+			rang = 50
+		}
+
+		i := 0
+		for len(strs) < rang {
+			if i == 0 {
+				strs = append(strs, "Asia/Tokyo")
+			}
+			if i % 2 == 0 {
+				strs = append(strs, "America/New_York")
+			} else if i % 3 == 0 {
+				strs = append(strs, "America/Los_Angeles")
+			}
+
+			i++
+
+		}
+
+		st := time.Now()
+		s := sprintSingleTz(strs, -1)
+
+		log.Printf("print single tz %v: %v\n", rang, s)
+		log.Printf("print single tz %v: total time: %v per func: %v\n", rang, time.Since(st), time.Since(st) / time.Duration(rang))
+		if s != "America/New_York" {
+			t.Error()
+		}
+	})
+
+	t.Run("print single tz 125:", func(t *testing.T) {
 		t.Parallel()
 		strs := []string{}
 
@@ -50,8 +84,8 @@ func TestSprintSingleTz(t *testing.T) {
 		st := time.Now()
 		s := sprintSingleTz(strs, -1)
 
-		log.Println("print single tz 50:", s)
-		log.Printf("print single tz 50: total time: %v per func: %v\n", time.Since(st), time.Since(st) / time.Duration(rang))
+		log.Printf("print single tz %v: %v\n", rang, s)
+		log.Printf("print single tz %v: total time: %v per func: %v\n", rang, time.Since(st), time.Since(st) / time.Duration(rang))
 		if s != "America/New_York" {
 			t.Error()
 		}
